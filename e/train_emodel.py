462df1cb4613c1948dd0ee31f871ba50caff7b67
Akshat <akshatjohri69@gmail.com>
2026-05-09 22:49:34 +0530

    minor changes

--- e/train_emodel.py
+++ e/train_emodel.py
@@ -73,6 +73,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = RandomForestClassifier(
     n_estimators=300,
     max_depth=20,
+    max_features="sqrt",
     min_samples_split=5,
     min_samples_leaf=2,
     random_state=42,
